d1e5fa6f68cd4905b310736a884ec5e859fa9b91
armeenrn <67667209+armeenrn@users.noreply.github.com>
2023-02-12 16:37:18 -0700

    Modified listener file

--- Project/Python/src/listener.py
+++ Project/Python/src/listener.py
@@ -4,74 +4,119 @@ import adafruit_board_toolkit.circuitpython_serial
 import sys
 import boto3
 import datetime
+import subprocess
+import os
 
+DMESG_CMD = "/usr/bin/dmesg"
+TAIL_CMD = "/usr/bin/tail"
+GREP_CMD = "/usr/bin/grep"
+CREATE_NESTED_DIR_CMD = "mkdir -p"
+MOUNT_CMD = "mount"
+CP_CMD = "cp"
+SYNC_CMD = "sync"
 
-comports = adafruit_board_toolkit.circuitpython_serial.data_comports()
-device_COM = '/dev/ttyACM11'
-lastTimeNoRead = None
-s3 = boto3.resource('s3')
-#if not comports:
- #   raise Exception("No modules found")
+PATTERN_1 = '''"sd.*: sd.*"'''
+PATTERN_2 = '''"sd.[0-9]"'''
 
-device = serial.Serial(device_COM, baudrate=115200)
-buffer = bytes()  
-try:
-    print("Got here")
-    while True:
-        if device.in_waiting > 0:
-            lastTimeNoRead = time.time()
-            response = device.read(device.in_waiting)
-            print(response);
-            if bytes("Out of memory", 'utf-8') in response:
-            	lastTimeNoRead = None
-            	buffer = bytes() 
-            	continue
-            if bytes("Should take image?", 'utf-8') in response:
-            	capture = input("Capture image?: ")
-            	if (capture == 'y'):
-            		device.write(bytes("c", 'utf-8'))
-            		lastTimeNoRead = None
-            		buffer = bytes() 
-            		continue
-            	elif (capture == 'n'):
-            		device.write(bytes("i", 'utf-8'))
-            		lastTimeNoRead = None
-            		buffer = bytes() 
-            		continue
-            	elif (capture == 'q'):
-            		device.write(bytes("q", 'utf-8'))
-            		lastTimeNoRead = None
-            		buffer = bytes() 
-            		continue
-            if bytes("done", 'utf-8') in response:
-                response = response.split(bytes("done", 'utf-8'))[0]
+DEV_DIR = "/dev"
+MNT_DIR = "/mnt/pico"
+
+UF2_FILE_PATH = '''./Project/C/build/src/Main/Main.uf2'''
+
+def listen():
+    comports = adafruit_board_toolkit.circuitpython_serial.data_comports()
+    device_COM = '/dev/ttyACM11'
+    lastTimeNoRead = None
+    s3 = boto3.resource('s3')
+    #if not comports:
+        #   raise Exception("No modules found")
+
+    device = serial.Serial(device_COM, baudrate=115200)
+    buffer = bytes()  
+    try:
+        print("Got here")
+        while True:
+            if device.in_waiting > 0:
+                lastTimeNoRead = time.time()
+                response = device.read(device.in_waiting)
+                print(response);
+                if bytes("Out of memory", 'utf-8') in response:
+                    lastTimeNoRead = None
+                    buffer = bytes() 
+                    continue
+                if bytes("Should take image?", 'utf-8') in response:
+                    capture = input("Capture image?: ")
+                    if (capture == 'y'):
+                        device.write(bytes("c", 'utf-8'))
+                        lastTimeNoRead = None
+                        buffer = bytes() 
+                        continue
+                    elif (capture == 'n'):
+                        device.write(bytes("i", 'utf-8'))
+                        lastTimeNoRead = None
+                        buffer = bytes() 
+                        continue
+                    elif (capture == 'q'):
+                        device.write(bytes("q", 'utf-8'))
+                        lastTimeNoRead = None
+                        buffer = bytes() 
+                        continue
+                if bytes("done", 'utf-8') in response:
+                    response = response.split(bytes("done", 'utf-8'))[0]
+                    buffer = buffer + response
+                    print("Got new picture!")
+                    image = open("saved1.jpeg", "wb+")
+                    image.write(buffer)
+                    image.close()
+                    image = open("saved1.jpeg", "rb+")
+                    imageName = "image_" + datetime.datetime.today().strftime("%d-%b-%Y-%H-%M-%S-%f") +  ".jpeg";
+                    s3.Bucket('arducambucket').put_object(Key=imageName, Body=image) 
+                    print("Sent image to S3 Bucket!")
+                    print(buffer)
+                    buffer = bytes()
+                    lastTimeNoRead = None
+                    print("Would you like to continue?")
+                    con = input("Continue: ")
+                    if (con == "y"):
+                        break
+                    else:
+                        continue
                 buffer = buffer + response
-                print("Got new picture!")
-                image = open("saved1.jpeg", "wb+")
-                image.write(buffer)
-                image.close()
-                image = open("saved1.jpeg", "rb+")
-                imageName = "image_" + datetime.datetime.today().strftime("%d-%b-%Y-%H-%M-%S-%f") +  ".jpeg";
-                s3.Bucket('arducambucket').put_object(Key=imageName, Body=image) 
-                print("Sent image to S3 Bucket!")
-                print(buffer)
-                buffer = bytes()
-                lastTimeNoRead = None
-                print("Would you like to continue?")
-                con = input("Continue: ")
-                if (con == "y"):
-                	break
-                else:
-                	continue
-                
-            buffer = buffer + response
-        elif lastTimeNoRead is None:
-            continue
-        elif (time.time() - lastTimeNoRead) < 20:
-            continue
-        else:
-            break
+            elif lastTimeNoRead is None:
+                continue
+            elif (time.time() - lastTimeNoRead) < 20:
+                continue
+            else:
+                break
     
 
-finally:
-    device.close()
+    finally:
+        device.close()
+
+def flash():
+     #Create tmp directory
+     subprocess.run([CREATE_NESTED_DIR_CMD, MNT_DIR], shell=True)
+     disk_file = subprocess.run([GREP_CMD + " -o " + PATTERN_2], stdout=subprocess.PIPE, input=subprocess
+               .run([GREP_CMD + " -o " + PATTERN_1], stdout=subprocess.PIPE, input=subprocess
+               .run([TAIL_CMD], stdout=subprocess.PIPE, input=subprocess
+                    .run([DMESG_CMD], stdout=subprocess.PIPE).stdout).stdout, shell=True).stdout, shell=True).stdout.decode("utf-8").split("\n")[0]
+     
+     sp = subprocess.run([MOUNT_CMD, DEV_DIR + "/" + disk_file, MNT_DIR], shell=True)
+     if sp.returncode != 0:
+          print("Error")
+          
+     if not os.access(UF2_FILE_PATH, os.X_OK):
+          print("UF2 File does not exist")
+    
+     subprocess.run([CP_CMD, UF2_FILE_PATH, MNT_DIR], shell=True)
+     subprocess.run([SYNC_CMD], shell=True)
+
+          
+def main():
+     # Flash UF2 on PICO
+     flash()
+     listen()
+
+
+if __name__ == "__main__":
+    main()
