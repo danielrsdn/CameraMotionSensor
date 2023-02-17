b97d02701c1ee5e1d9bd8989c0d10ed74426beb3
armeenrn <67667209+armeenrn@users.noreply.github.com>
2023-02-16 21:01:00 -0700

    malloc

--- Project/C/src/Main/Main.cpp
+++ Project/C/src/Main/Main.cpp
@@ -161,7 +161,7 @@ int main()
     newAverageDistance = distance*0.80 + inst_distance*0.20;
     double diff = absDifference(newAverageDistance, distance)/distance*100.0;
     std::string s = std::to_string(diff);
-    uint8_t* messageBuff = (uint8_t*) malloc(7*sizeof(uint8_t));
+    uint8_t* messageBuff = (uint8_t*) malloc(s.size()*sizeof(uint8_t));
     convertCharToUInt8(s.c_str(),s.size(), messageBuff); 
     SerialUsb(messageBuff, s.size());
     free(messageBuff);
