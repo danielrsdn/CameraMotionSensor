147dc370794b6bf0cf75945e08711c4d947271a2
armeenrn <67667209+armeenrn@users.noreply.github.com>
2023-02-19 21:18:26 -0700

    increase burt size

--- Project/Python/src/listener.py
+++ Project/Python/src/listener.py
@@ -171,7 +171,7 @@ def start():
 
     device = serial.Serial(device_COM, baudrate=115200)
     imageHandler = ImageHandler()
-    listener = Listener(device, imageHandler, 5)
+    listener = Listener(device, imageHandler, 30)
     try: 
         listener.listen()
     finally:
